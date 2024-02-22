3dc9b77f99f69045568c0a750f39c94e143ea891
Massamba THIOYE <massbatht@gmail.com>
2024-02-22 10:35:48 +0000

    first commit

---
+++ app/src/main/java/sn/esmt/scolarite/ListeEtudiantActivity.kt
@@ -0,0 +1,11 @@
+package sn.esmt.scolarite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListeEtudiantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liste_etudiant)
+    }
+}
